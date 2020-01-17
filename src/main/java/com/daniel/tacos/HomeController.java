b772b4af08134613d39a022a4f09ca12f898eb1c
szefoski <szefoski22@gmail.com>
2020-01-17 15:31:11 +0100

    test

--- src/main/java/com/daniel/tacos/HomeController.java
+++ src/main/java/com/daniel/tacos/HomeController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-//test
     @GetMapping("/")
     public String home() {
         return "home";
