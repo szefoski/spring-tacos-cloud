44f57c79669c77511d65e1f594f804362e0cd0fc
szefoski <szefoski22@gmail.com>
2020-01-29 23:08:35 +0100

    Sonar Cloud configuration update

--- src/main/java/com/daniel/tacos/HomeController.java
+++ src/main/java/com/daniel/tacos/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-	//Test
+	//Testa1
     @GetMapping("/")
     public String home() {
         return "home";
